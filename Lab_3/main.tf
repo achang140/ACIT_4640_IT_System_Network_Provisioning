a0ad6b8c521797ef2d378e1d3f98f57f08d71054
Amanda Chang <achang140@my.bcit.ca>
2024-01-28 10:36:21 -0800

    Commit

--- Lab_3/main.tf
+++ Lab_3/main.tf
@@ -1,3 +1,4 @@
+# Terraform Provider - Terraform can download and use the correct provider plugin 
 terraform {
   required_providers {
     aws = {
@@ -7,7 +8,11 @@ terraform {
   }
 }
 
+# Configure the AWS Provider with the region to use 
 provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile = "acit4640_admin"
   region = "us-west-2"
 }
 
@@ -16,6 +21,7 @@ variable "base_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
+# Create a VPC named main 
 resource "aws_vpc" "main" {
   cidr_block       = var.base_cidr_block
   instance_tenancy = "default"
@@ -25,6 +31,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Creates a Subnet named main within the VPC 
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -36,6 +43,8 @@ resource "aws_subnet" "main" {
   }
 }
 
+# Creates an Internet Gateway named main that attaches to the VPC 
+# Allows traffic to and from the internet 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -43,6 +52,7 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+# Creates a Route Table named main that associates with the VPC 
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -51,12 +61,16 @@ resource "aws_route_table" "main" {
   }
 }
 
+# Creates a Route named default_route that is added to the Route Table 
+# All traffic with the destination CIDR block of 0.0.0.0/0 (Any IP Address) should be routed to the Internet Gateway 
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.main.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
 
+# Creates an association between the Subnet and the Route Table
+# The subnet will use the Route Table for routing its traffic  
 resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
@@ -64,20 +78,46 @@ resource "aws_route_table_association" "main" {
 
 # security group for instances COMPLETE ME!
 resource "aws_security_group" "main" {
-  
+  name   = "main"
+  vpc_id = aws_vpc.main.id 
 }
 
 # security group egress rules COMPLETE ME!
-resource "aws_vpc_security_group_egress_rule" "main" {
+resource "aws_security_group_rule" "main_egress" {
 # make this open to everything from everywhere
+  type              = "egress"
+  security_group_id = aws_security_group.main.id 
+  from_port         = 0 
+  to_port           = 0 
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 # security group ingress rules COMPLETE ME!
-resource "aws_vpc_security_group_ingress_rule" "main" {
-# ssh and http in from everywhere
+resource "aws_security_group_rule" "main_ingress_ssh" {
+# ssh in from everywhere
+  type              = "ingress"
+  security_group_id = aws_security_group.main.id
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp" 
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
-# get the most recent ami for Ubuntu 22.04
+resource "aws_security_group_rule" "main_ingress_http" {
+# http in from everywhere
+  type              = "ingress"
+  security_group_id = aws_security_group.main.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp" 
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+# Get the most recent ami for Ubuntu 22.04
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -91,20 +131,20 @@ data "aws_ami" "ubuntu" {
 # key pair from local key COMPLETE ME!
 resource "aws_key_pair" "local_key" {
   key_name   = "4640-Key"
-  public_key = file()
+  public_key = file("/home/amanda/.ssh/4640-Key.pub")
 }
 
 # ec2 instance COMPLETE ME!
 resource "aws_instance" "ubuntu" {
   instance_type = "t2.micro"
-  ami           = data.
+  ami           = data.aws_ami.ubuntu.id
   
   tags = {
     Name = "ubuntu-server"
   }
 
-  key_name               =
-  vpc_security_group_ids = 
+  key_name               = aws_key_pair.local_key.key_name
+  vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id              = aws_subnet.main.id
 
   root_block_device {
@@ -116,5 +156,3 @@ resource "aws_instance" "ubuntu" {
 output "instance_public_ips" {
   value = aws_instance.ubuntu.public_ip
 }
-
-
